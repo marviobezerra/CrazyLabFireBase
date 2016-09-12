639821374e2a122ae842cd847216053fb8fdbf58
Márvio Bezerra <marvio.bezerra@hotmail.com>
2016-09-12 02:14:03 -0300

    Working on auth

--- app/components/user/user.component.ts
+++ app/components/user/user.component.ts
@@ -1,10 +1,33 @@
 import { Component } from "@angular/core";
+import { FirebaseAuth, FirebaseAuthState, AuthMethods, AuthProviders } from "angularfire2";
 
 @Component({
-	selector: "user",
+    selector: "user",
     templateUrl: "/components/user/user.html",
     styles: [require("./user.scss")]
 })
 export class UserComponent {
-    constructor() {}
+    constructor(public auth$: FirebaseAuth) {
+
+        auth$
+            .subscribe((state: FirebaseAuthState) => {
+                this.authState = state;
+            });
+
+    }
+
+    public authState: FirebaseAuthState = null;
+
+    get Authenticated(): boolean {
+        return this.authState !== null;
+    }
+
+    private signIn(provider: number): firebase.Promise<FirebaseAuthState> {
+        return this.auth$.login({ method: AuthMethods.Popup, provider: AuthProviders.Google })
+            .catch(error => console.log('ERROR @ AuthService#signIn() :', error));
+    }
+
+    public Login(): firebase.Promise<FirebaseAuthState> {
+        return this.signIn(AuthProviders.Google);
+    }
 }
